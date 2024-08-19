e65f677672696766e4581ed6e97cb63acbff4fe3
Dmitry Orlov <dmitry.orlov@novopackbb.ru>
2024-08-19 14:45:07 +0300

    Worked example

---
+++ src/components/SomeComponent.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import { Container, Button } from "react-bootstrap"
+import ModalFirst from "./modals/ModalFirst"
+import ModalSecond from "./modals/ModalSecond"
+import ModalThird from "./modals/ModalThird"
+import { useDialog } from "../hooks/useDialog"
+
+const SomeComponent = () => {
+
+  const dialog = useDialog()
+
+  const [result, setResult] = useState('')
+
+  const onButtonClick = async () => {
+
+    setResult('')
+
+    let result = 'Всё плохо.'
+
+    if (
+      await dialog.show(<ModalFirst close={dialog.close} />) &&
+      await dialog.show(<ModalSecond {...dialog} />) &&
+      await dialog.show(<ModalThird {...dialog} />)
+    )
+      result = 'Отлично!'
+
+    setResult(result)
+  }
+
+  return (
+    <Container>
+      <h1>Demo Modal dialog</h1>
+      <p>This is a demo of a modal dialog</p>
+      <p>Click the button to open the modal</p>
+      <Button variant="primary" onClick={onButtonClick}>Open Modal</Button>
+
+      <p className='mt-3'>{result}</p>
+
+      
+    </Container>
+  )
+}
+
+export default SomeComponent
